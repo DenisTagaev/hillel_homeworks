8bd60e164f1e0bfbf4652b8281f98e80ae3562d3
DenisTagaev <78323055+DenisTagaev@users.noreply.github.com>
2021-02-13 16:56:36 +0200

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -1,58 +1,48 @@
 let a1 = prompt("Enter the first number/s 0-9"),
-    a2,
-    operator,
-    result;
 
 function isNumber(value) {
-    return !isNaN(parseFloat(value)) && isFinite(value)
+  return !isNaN(parseFloat (value) ) && isFinite(value);
 }
 
-if(a1 === null) {
-    alert("No data");
+if (a1 === null) {
+    alert("You cancelled");
 } 
-//Тут прервется выполнение если нет ввода
-
-else if(!isNumber (a1) ) {
-    alert("Only numbers allowed!");
-    //Тут прервет выволнение если в первый промт введено не число    
+else if (!isNumber (a1) ) {
+    alert("Only numbers allowed!");    
 } else {
-   a2 = prompt("Enter more number/s 0-9");
+
+    let a2 = prompt("Enter more number/s 0-9");
     
-    if(a2 === null) {
-       alert("No data");
-       //Тут прервется выполнение если нет ввода
-    }
-    
-    else if(!isNumber (a2) ) {
-        alert("forbidden symbols");
-        //Тут прервет выволнение если во второй промт введено не число
+    if (a2 === null) {
+       alert("You cancelled");
+    } 
+    else if (!isNumber (a2) ) {
+        alert("Only numbers allowed!");
     } else {
-        operator = prompt("Choose operator +, -, /, *");  
-          switch(operator){
-             case "+" :
-                 result = +a1 + +a2;
-                 alert(result);
-                 break;
+
+        let operator = prompt("Choose operator +, -, /, *"); 
+        let result;
+        
+        switch (operator) {
+          case "+" :
+            result = +a1 + +a2;
+            break;
                  
-             case "-" :
-                 result = a1 - a2;
-                 alert(result);
-                 break;
+          case "-" :
+            result = a1 - a2;
+            break;
                   
-             case "/" :
-                 result = a1 / a2;
-                 alert(result);
-                 break;
+          case "/" :
+            result = a1 / a2;
+            break;
                   
-             case "*" :
-                 result = a1 * a2;
-                 alert(result);
-                 //тут выведет результат если все вводы корректны
-                 break;
+          case "*" :
+            result = a1 * a2;
+            break;
                 
-              default:
-                alert("Incorrect operation");
-                //тут выйдет ошибка если оператор не подходит
-                }
-            }
-}
+          default:
+            alert("Incorrect operation");
+        }
+        alert(result);
+    }
+  }
